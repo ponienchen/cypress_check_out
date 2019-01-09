e3120bf0062736af1c77f4872c22c14beec63a56
pchen <pchen@indeed.com>
2019-01-09 10:03:00 -0800

    cypress sample codes initial commit

---
+++ cypress/integration/sample_spec.js
@@ -0,0 +1,9 @@
+/// <reference types="Cypress" />
+
+describe('My First Test', function(){
+	it('Does not do much', function(){
+		cy.visit('https://example.cypress.io')
+		cy.contains('type').click()
+		
+	})
+})
